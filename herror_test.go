4f873549296b347d834ee3f219691dcbae248fd7
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro.local>
2018-06-13 22:02:57 +0900

    Add some errors

--- herror_test.go
+++ herror_test.go
@@ -35,3 +35,43 @@ func TestInternalServerError(t *testing.T) {
 	assert.Regexp(t, `^.+herror_test\.go:30$`, herr.Call())
 	assert.Nil(t, herr.Error())
 }
+
+func TestUnauthorizedBadAccessToken(t *testing.T) {
+	herr := NewUnauthorizedBadAccessToken(nil)
+	assert.Equal(t, 401, herr.Code())
+	assert.Equal(t, "Bad access token", herr.PublicMessage())
+	assert.Equal(t, "Bad access token", herr.PrivateMessage())
+	assert.True(t, herr.Is4XX())
+	assert.Regexp(t, `^.+herror_test\.go:40$`, herr.Call())
+	assert.Nil(t, herr.Error())
+}
+
+func TestBindJSONError(t *testing.T) {
+	herr := NewBindJSONError(nil)
+	assert.Equal(t, 400, herr.Code())
+	assert.Equal(t, "Cannot bind body", herr.PublicMessage())
+	assert.Equal(t, "Cannot bind body", herr.PrivateMessage())
+	assert.True(t, herr.Is4XX())
+	assert.Regexp(t, `^.+herror_test\.go:50$`, herr.Call())
+	assert.Nil(t, herr.Error())
+}
+
+func TestInvalidParameterError(t *testing.T) {
+	herr := NewInvalidParameterError("pri", "pub", nil)
+	assert.Equal(t, 400, herr.Code())
+	assert.Equal(t, "pri", herr.PublicMessage())
+	assert.Equal(t, "pub", herr.PrivateMessage())
+	assert.True(t, herr.Is4XX())
+	assert.Regexp(t, `^.+herror_test\.go:60$`, herr.Call())
+	assert.Nil(t, herr.Error())
+}
+
+func TestConflictError(t *testing.T) {
+	herr := NewConflictError("pri", "pub", nil)
+	assert.Equal(t, 409, herr.Code())
+	assert.Equal(t, "pri", herr.PublicMessage())
+	assert.Equal(t, "pub", herr.PrivateMessage())
+	assert.True(t, herr.Is4XX())
+	assert.Regexp(t, `^.+herror_test\.go:70$`, herr.Call())
+	assert.Nil(t, herr.Error())
+}
